0ae6d6f4c7abab07b2f4ab48a6fc95b19446e7de
AllisonRoss413 <148769460+AllisonRoss413@users.noreply.github.com>
2023-11-01 17:08:31 -0400

    Delete story_content directory

--- story_content/6ntfPg7d35R_transcripts.js
+++
@@ -1,5 +0,0 @@
-
-(function() {
-    const data = {"transcripts":[{"name":"captions","cues":[]}]};
-    window.globalLoadJsAsset('story_content/6ntfPg7d35R_transcripts.js', JSON.stringify(data));
-})();
